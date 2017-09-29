8302624920aad337e39dfd9ff7fe6a0a5dcf6a44
JOKER XU <18696790771@163.com>
2017-09-30 00:36:54 +0800

    新增字符串

---
+++ 04-string/01-string-api.js
@@ -0,0 +1,5 @@
+const s = 'Hello world!';
+console.warn(s.startsWith('Hello')); // true
+console.warn(s.endsWith('!')); // true
+console.warn(s.includes('o')); // true
+console.warn(s.indexOf('o1')); // -1
---
+++ 04-string/03-string-api.js
@@ -0,0 +1,9 @@
+console.warn('x'.repeat(3)); // "xxx"
+console.warn('Hello'.repeat(2)); // "HelloHello"
+console.warn('na'.repeat(0));   // ""
+
+console.warn('na'.repeat(2.9)); // "nana";
+console.warn('na'.repeat(-2.9)); // RangeError: Invalid count value;
+console.warn('na'.repeat(Infinity)); // RangeError: Invalid count value;
+console.warn('na'.repeat(-0.9)); // "";
+console.warn('ha'.repeat('2')); // haha;
