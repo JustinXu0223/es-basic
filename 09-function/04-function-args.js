d446c91c14186e5497218e329fed66e98d2c1b0f
JOKER XU <18696790771@163.com>
2018-03-12 00:00:04 +0800

    callback update

--- 09-function/04-function-args.js
+++ 09-function/04-function-args.js
@@ -1,10 +1,22 @@
 function foo() {
     setTimeout( () => {
         console.log("args:", arguments);
+        if (arguments.length === 1) {
+            console.log('this is one params')
+            return false
+        }
+        if (arguments.length === 2) {
+            console.log('this is two params')
+            return false
+        }
+        if (arguments.length === 3) {
+            console.log('this is three params')
+            return false
+        }
     },100);
 }
 
-foo(1,2,3,4);
+foo(1,2,3);
 // args: { '0': 1, '1': 2, '2': 3, '3': 4 }
 
 const func = (...args) => console.log(args);
