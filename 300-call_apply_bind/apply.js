54967a6b6d2c1d7a1094a8c6ca40a609c7418a51
JUSTIN XU <18696790771@163.com>
2018-06-15 09:30:48 +0800

    add call code

---
+++ 300-call_apply_bind/apply.js
@@ -0,0 +1,6 @@
+/**
+ * @component apply.js
+ * @description
+ * @time 2018/6/14
+ * @author JUSTIN XU
+ */
---
+++ 300-call_apply_bind/call.js
@@ -0,0 +1,70 @@
+/**
+ * @component call.js
+ * @description call 原理解析
+ * @time 2018/6/14
+ * @author JUSTIN XU
+ */
+
+function add(c, d) {
+  return this.a + this.b + c + d;
+}
+
+const obj = { a: 1, b: 2 };
+
+// console.error(add.call(obj, 3, 4));
+// add.call(o, 5, 7)使add函数中的this指向o对象，
+// 并且执行了add函数。依据上例把o对象改造成如下：
+
+const o = {
+  a: 1,
+  b: 2,
+  add: function(c, d) {
+    return this.a + this.b + c + d
+  }
+};
+
+// console.log(o.add(3, 4));
+
+// 给o对象添加一个add属性，这个时候 this 就指向了 o，
+// o.add(5,7)得到的结果和add.call(o, 5, 6)相同。
+// 但是给对象o添加了一个额外的add属性，这个属性我们是不需要的，所以可以使用delete删除它。
+
+// 所以我们使用js实现call方法的步骤可以分为：
+
+// 1. 将函数设为对象的属性
+  // o.fn = bar
+// 2. 执行该函数
+  // o.fn()
+// 3. 删除该函数
+  // delete o.fn
+
+// ES3 call 实现
+Function.prototype.es3Call = function (context) {
+  var content = context || window;
+  content.fn = this;
+  // var arr = Array.prototype.slice.call(arguments, 1);
+  // console.log(JSON.stringify(arr));
+  var args = [];
+  for (var i = 1, len = arguments.length ; i < len; i++) {
+    args.push(arguments[i]);
+  }
+  var result = eval('content.fn('+args+')');
+  delete content.fn;
+  return result;
+}
+
+console.error(add.es3Call(obj, 3, 4));
+
+// ES5 call 实现
+Function.prototype.es6Call = function (context) {
+  var context = context || window;
+  context.fn = this;
+  var args = [];
+  for (var i = 1, len = arguments.length; i < len; i++) {
+    args.push(arguments[i]);
+  }
+  const result = context.fn(...args);
+  return result;
+}
+
+console.log(add.es6Call(obj, 3, 4));
