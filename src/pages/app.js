7db69b914cd35716481f74bfc567c16a071d5438
justin <justinxu0223@gmail.com>
2019-08-19 20:49:28 +0800

    Added crypto-js demo

--- src/pages/app.js
+++ src/pages/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Base64 } from 'js-base64';
+// import { Base64 } from 'js-base64';
 
 // static source
 import logo from '../assets/logo.svg';
@@ -26,69 +26,13 @@ import logo from '../assets/logo.svg';
 // import TabPane from './components/tabs/tabPane';
 
 // oop
-import { Demo1, Demo2 } from './demo/demo';
+// import { Demo1, Demo2 } from './demo/demo';
+
+// utils
+import { print } from '../utils/sign';
 
 // style
 import './app.css';
-
-const aList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
-const bList = '0123456789'.split('');
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function getSuffix(num = 2) {
-  // return Array.form(Array(num)).map((v, i) => {
-  //   if (i % 2 === 0) return aList[getRandomInt(51)];
-  //   return bList[getRandomInt(10)];
-  // });
-  const list = [];
-  for (let i = 0; i < num; i += 1) {
-    if (i % 2 === 0) {
-      list.push(aList[getRandomInt(51)]);
-    } else {
-      list.push(bList[getRandomInt(10)]);
-    }
-  }
-  return list.join('');
-}
-
-const list = [0, 1, 3, 2, 1];
-
-function encode(data, arr = list) {
-  try {
-    let res = data;
-    arr.forEach((item) => {
-      if (!item) {
-        res = Base64.encode(JSON.stringify(data));
-      } else {
-        res = Base64.encode(`${getSuffix(item)}${res}`);
-      }
-      console.log(res);
-    });
-    return res;
-  } catch (e) {
-    return '';
-  }
-}
-
-function decode(data, arr = list.reverse()) {
-  try {
-    let res = data;
-    arr.forEach((item) => {
-      if (!item) {
-        res = JSON.parse(Base64.decode(res));
-      } else {
-        res = Base64.decode(res).slice(item);
-      }
-      console.log(res);
-    });
-    return res || {};
-  } catch (e) {
-    return {};
-  }
-}
 
 class App extends React.Component {
   state = {};
@@ -113,10 +57,7 @@ class App extends React.Component {
   // };
 
   componentDidMount() {
-    const data = encode({ name: '12312adc1', password: '321212' });
-    console.log('@encode-result:', data);
-    const res = decode(data);
-    console.log('@decode-result:', res);
+    print();
   }
 
   render() {
@@ -162,8 +103,8 @@ class App extends React.Component {
               <div>内容3</div>
             </TabPane>
           </Tabs> */}
-          <Demo1 />
-          <Demo2 />
+          {/* <Demo1 />
+          <Demo2 /> */}
         </header>
       </div>
     );
